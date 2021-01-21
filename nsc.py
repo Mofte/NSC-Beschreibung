480204acef9d299651454a52edb713010c1680a8
Mofte <github@mofte.net>
2021-01-21 14:31:07 +0100

    Create nsc.py
    Switched from copy&paste Javascript to my own Python code.

---
+++ nsc.py
@@ -0,0 +1,20 @@
+import random
+
+alter = ["jung", "mittelalt", "alt"]
+geschlecht = ["männlich", "weiblich"]
+attribute = open("attribute.txt").readlines()
+berufe = open("berufe.txt").readlines()
+
+print('Vor euch steht eine Person, sie ist\n')
+
+print(alter[random.randint(0, len(alter)-1)] + '\n')
+
+print(geschlecht[random.randint(0, len(geschlecht)-1)] + '\n')
+
+print(attribute[random.randint(0, len(attribute)-1)])
+
+print(attribute[random.randint(0, len(attribute)-1)])
+
+print('und von Beruf\n')
+
+print(berufe[random.randint(0, len(berufe)-1)])
